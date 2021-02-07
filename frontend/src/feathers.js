84480005796622b10316ecceb4033101d6321bfc
apmis <simpa@apmis.ng>
2021-02-07 01:57:51 +0100

    api check 2

--- frontend/src/feathers.js
+++ frontend/src/feathers.js
@@ -3,9 +3,10 @@ import feathers from '@feathersjs/feathers'
 import socketio from '@feathersjs/socketio-client'
 import authentication from '@feathersjs/authentication-client'
 
+const herok= 'https://healthstack-backend.herokuapp.com'
 const url= 'http://localhost:3035'
 const API = process.env.NODE_ENV === 'production' ? 'https://healthstack-backend.herokuapp.com' : url;
-const socket=io(API, {
+const socket=io(herok, {
     transports: ['websocket'],
     forceNew: true
   })
