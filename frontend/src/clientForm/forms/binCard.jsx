d1dc965fad7288c9235ed81eb5c2c2c75898671b
arabambi <arababs2015@gmail.com>
2022-03-09 14:23:09 +0100

    init

--- frontend/src/clientForm/forms/binCard.jsx
+++ frontend/src/clientForm/forms/binCard.jsx
@@ -4,103 +4,115 @@ import { useForm } from "react-hook-form";
 
 const BinCard = ({onSubmit}) => {
   const { register, handleSubmit } = useForm(); 
+
   return (
-    <div className="card">
-      <div className="card-header">
-        <p className="card-header-title">Bin Card</p>
-      </div>
-      <div className="card-content vscrollable">
-        <div className="field-body">
-          <div className="field">
-            <label className="label is-small">Item Description</label>
-            <div className="control">
-              <textarea className="textarea is-small"></textarea>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="card">
+        <div className="card-header">
+          <p className="card-header-title">Bin Card</p>
+        </div>
+        <div className="card-content vscrollable">
+          <div className="field-body">
+            <div className="field">
+              <label className="label is-small">Item Description</label>
+              <div className="control">
+                <textarea className="textarea is-small"></textarea>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="field-body mt-3">
-          <div className="field">
-            <label className="label is-small">Date</label>
-            <div className="control">
-              <input className="input is-small" type="date" />
+
+          <div className="field-body mt-3">
+            <div className="field">
+              <label className="label is-small">Date</label>
+              <div className="control">
+                <input className="input is-small" type="date" />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="field-body mt-3">
-          <div className="field">
-            <label className="label is-small">Supplier</label>
-            <p className="control is-expanded">
+
+          <div className="field-body mt-3">
+            <div className="field">
+              <label className="label is-small">Supplier</label>
+              <p className="control is-expanded">
+                <input className="input is-small" type="text" />
+              </p>
+            </div>
+          </div>
+
+          <div className="grid mt-3">
+            <div className="field-body">
+              <div className="field">
+                <label className="label is-small">Invoice No</label>
+                <p className="control is-expanded">
+                  <input className="input is-small" type="number" />
+                </p>
+              </div>
+            </div>
+            <div className="field-body">
+              <div className="field">
+                <label className="label is-small">Quantity Received</label>
+                <p className="control is-expanded">
+                  <input className="input is-small" type="number" />
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="grid mt-3">
+            <div className="field w-100">
+              <label className="label is-small">Quantity Issued</label>
+              <p className="control">
+                <input className="input is-small" type="number" />
+              </p>
+            </div>
+            <div className="field w-100">
+              <label className="label is-small">Balance</label>
+              <p className="control">
+                <input className="input is-small" type="number" />
+              </p>
+            </div>
+          </div>
+
+          <div className="grid mt-3">
+            <div className="field-body w-100">
+              <div className="field">
+                <label className="label is-small">Batch Date</label>
+                <div className="control">
+                  <input className="input is-small" type="date" />
+                </div>
+              </div>
+            </div>
+            <div className="field-body w-100">
+              <div className="field w-100">
+                <label className="label is-small">Expiry Date</label>
+                <div className="control">
+                  <input className="input is-small" type="date" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="field w-100 mt-3">
+            <label className="label is-small">Remark</label>
+            <p className="control">
               <input className="input is-small" type="text" />
             </p>
           </div>
-        </div>
-        <div className="grid mt-3">
-          <div className="field-body">
-            <div className="field">
-              <label className="label is-small">Invoice No</label>
-              <p className="control is-expanded">
-                <input className="input is-small" type="number" />
-              </p>
-            </div>
-          </div>
-          <div className="field-body">
-            <div className="field">
-              <label className="label is-small">Quantity Received</label>
-              <p className="control is-expanded">
-                <input className="input is-small" type="number" />
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="grid mt-3">
-          <div className="field w-100">
-            <label className="label is-small">Quantity Issued</label>
+
+          <div className="field w-100 mt-3">
+            <label className="label is-small">Sign</label>
             <p className="control">
-              <input className="input is-small" type="number" />
+              <input className="input is-small" type="text" />
             </p>
           </div>
-          <div className="field w-100">
-            <label className="label is-small">Balance</label>
-            <p className="control">
-              <input className="input is-small" type="number" />
-            </p>
+
+          <div className="field mt-4">
+            <button className="button is-success is-small">Submit Form</button>
           </div>
-        </div>
-        <div className="grid mt-3">
-          <div className="field-body w-100">
-            <div className="field">
-              <label className="label is-small">Batch Date</label>
-              <div className="control">
-                <input className="input is-small" type="date" />
-              </div>
-            </div>
-          </div>
-          <div className="field-body w-100">
-            <div className="field w-100">
-              <label className="label is-small">Expiry Date</label>
-              <div className="control">
-                <input className="input is-small" type="date" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="field w-100 mt-3">
-          <label className="label is-small">Remark</label>
-          <p className="control">
-            <input className="input is-small" type="text" />
-          </p>
-        </div>
-        <div className="field w-100 mt-3">
-          <label className="label is-small">Sign</label>
-          <p className="control">
-            <input className="input is-small" type="text" />
-          </p>
-        </div>
-        <div className="field mt-4">
-          <button className="button is-success is-small">Submit Form</button>
+          
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
