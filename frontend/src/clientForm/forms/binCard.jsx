a73f1f69ca221231418d44c2c775de0b44ff4adb
arabambi <arababs2015@gmail.com>
2022-03-09 14:50:42 +0100

    bin card done

--- frontend/src/clientForm/forms/binCard.jsx
+++ frontend/src/clientForm/forms/binCard.jsx
@@ -16,7 +16,7 @@ const BinCard = ({onSubmit}) => {
             <div className="field">
               <label className="label is-small">Item Description</label>
               <div className="control">
-                <textarea className="textarea is-small"></textarea>
+                <textarea ref={register} name="itemDescription" className="textarea is-small" type="text"></textarea>
               </div>
             </div>
           </div>
@@ -25,7 +25,7 @@ const BinCard = ({onSubmit}) => {
             <div className="field">
               <label className="label is-small">Date</label>
               <div className="control">
-                <input className="input is-small" type="date" />
+                <input ref={register} name="date" className="input is-small" type="date" />
               </div>
             </div>
           </div>
@@ -34,7 +34,7 @@ const BinCard = ({onSubmit}) => {
             <div className="field">
               <label className="label is-small">Supplier</label>
               <p className="control is-expanded">
-                <input className="input is-small" type="text" />
+                <input ref={register} name="supplier" className="input is-small" type="text" />
               </p>
             </div>
           </div>
@@ -44,7 +44,7 @@ const BinCard = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Invoice No</label>
                 <p className="control is-expanded">
-                  <input className="input is-small" type="number" />
+                  <input ref={register} name="invoiceNo" className="input is-small" type="number" />
                 </p>
               </div>
             </div>
@@ -52,7 +52,7 @@ const BinCard = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Quantity Received</label>
                 <p className="control is-expanded">
-                  <input className="input is-small" type="number" />
+                  <input ref={register} name="quantityReceived" className="input is-small" type="number" />
                 </p>
               </div>
             </div>
@@ -62,13 +62,13 @@ const BinCard = ({onSubmit}) => {
             <div className="field w-100">
               <label className="label is-small">Quantity Issued</label>
               <p className="control">
-                <input className="input is-small" type="number" />
+                <input ref={register} name="qnatityIssued" className="input is-small" type="number" />
               </p>
             </div>
             <div className="field w-100">
               <label className="label is-small">Balance</label>
               <p className="control">
-                <input className="input is-small" type="number" />
+                <input ref={register} name="balance" className="input is-small" type="number" />
               </p>
             </div>
           </div>
@@ -78,7 +78,7 @@ const BinCard = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Batch Date</label>
                 <div className="control">
-                  <input className="input is-small" type="date" />
+                  <input ref={register} name="batchDate" className="input is-small" type="date" />
                 </div>
               </div>
             </div>
@@ -86,7 +86,7 @@ const BinCard = ({onSubmit}) => {
               <div className="field w-100">
                 <label className="label is-small">Expiry Date</label>
                 <div className="control">
-                  <input className="input is-small" type="date" />
+                  <input ref={register} name="ExpiryDate" className="input is-small" type="date" />
                 </div>
               </div>
             </div>
@@ -95,21 +95,21 @@ const BinCard = ({onSubmit}) => {
           <div className="field w-100 mt-3">
             <label className="label is-small">Remark</label>
             <p className="control">
-              <input className="input is-small" type="text" />
+              <input ref={register} name="remark" className="input is-small" type="text" />
             </p>
           </div>
 
           <div className="field w-100 mt-3">
             <label className="label is-small">Sign</label>
             <p className="control">
-              <input className="input is-small" type="text" />
+              <input ref={register} name="sign" className="input is-small" type="text" />
             </p>
           </div>
 
           <div className="field mt-4">
             <button className="button is-success is-small">Submit Form</button>
           </div>
-          
+
         </div>
       </div>
     </form>
