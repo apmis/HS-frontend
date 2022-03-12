2a192fd1373af5ac0a8f2aa06bff3c9b2eb34573
arabambi <arababs2015@gmail.com>
2022-03-12 10:05:38 +0100

    discharge summary

--- frontend/src/clientForm/forms/dischargeSummary.jsx
+++ frontend/src/clientForm/forms/dischargeSummary.jsx
@@ -16,7 +16,7 @@ const DischargeSummary = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Date of Admission</label>
                 <div className="control">
-                  <input className="input is-small" type="date" />
+                  <input ref={register} name="dateOfAdmin" className="input is-small" type="date" />
                 </div>
               </div>
             </div>
@@ -24,7 +24,7 @@ const DischargeSummary = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Date of Discharge</label>
                 <div className="control">
-                  <input className="input is-small" type="date" />
+                  <input ref={register} name="dateOfDischarge" className="input is-small" type="date" />
                 </div>
               </div>
             </div>
@@ -32,19 +32,19 @@ const DischargeSummary = ({onSubmit}) => {
           <div className="field">
             <label className="label is-small">Discharge Diagnoses</label>
             <div className="control">
-              <textarea className="textarea is-small"></textarea>
+              <textarea ref={register} name="dischargeDiagnosis" className="textarea is-small"></textarea>
             </div>
           </div>
           <div className="field">
             <label className="label is-small">Consultant</label>
             <p className="control is-expanded">
-              <input className="input is-small" type="text" />
+              <input ref={register} name="consultant" className="input is-small" type="text" />
             </p>
           </div>
           <div className="field">
             <label className="label is-small">Procedures</label>
             <div className="control">
-              <textarea className="textarea is-small"></textarea>
+              <textarea ref={register} name="procedures" className="textarea is-small"></textarea>
             </div>
           </div>
           <div className="field">
@@ -54,15 +54,15 @@ const DischargeSummary = ({onSubmit}) => {
             <p className="">
               {" "}
               This patient is an{" "}
-              <input type="text" name="" id="" className="dotted_bottom" />{" "}
+              <input ref={register} name="age" type="text" id="" className="dotted_bottom" />{" "}
               year-old male/female history of: <br />
-              <textarea className="textarea is-small mt-3"></textarea>
+              <textarea ref={register} name="history" className="textarea is-small mt-3"></textarea>
             </p>
           </div>
           <div className="field">
             <label className="label is-small">Diagnostic Studies</label>
             <div className="control">
-              <textarea className="textarea is-small"></textarea>
+              <textarea ref={register} name="diagStudies" className="textarea is-small"></textarea>
             </div>
           </div>
           <div className="field">
@@ -71,40 +71,40 @@ const DischargeSummary = ({onSubmit}) => {
             </label>
             <label className="label is-small">Discharge Disposition</label>
             <label class="checkbox">
-              <input type="checkbox" />
+              <input ref={register} name="dischargeDisposition" type="checkbox" />
               <span className="ms-2 is-small">Discharge Home</span>
             </label>
             <label class="checkbox ms-3">
-              <input type="checkbox" />
+              <input ref={register} name="teachingHosp" type="checkbox" />
               <span className="ms-2 is-small">To a teaching hospital</span>
             </label>
             <label class="checkbox ms-3">
-              <input type="checkbox" />
+              <input ref={register} name="facilityAbroad" type="checkbox" />
               <span className="ms-2 is-small">To a facility abroad</span>
             </label>
           </div>
           <div className="field">
             <label className="label is-small">Activity Prescription</label>
             <div className="control">
-              <textarea className="textarea is-small"></textarea>
+              <textarea ref={register} name="activePrescription" className="textarea is-small"></textarea>
             </div>
           </div>
           <div className="field">
             <label className="label is-small">Diet Prescribed</label>
             <div className="control">
-              <textarea className="textarea is-small"></textarea>
+              <textarea ref={register} name="dietPrescribed" className="textarea is-small"></textarea>
             </div>
           </div>
           <div className="field">
             <label className="label is-small">Medications</label>
             <div className="control">
-              <textarea className="textarea is-small"></textarea>
+              <textarea ref={register} name="medications" className="textarea is-small"></textarea>
             </div>
           </div>
           <div className="field">
             <label className="label is-small">Follow- up</label>
             <div className="control">
-              <textarea className="textarea is-small"></textarea>
+              <textarea ref={register} name="followUp" className="textarea is-small"></textarea>
             </div>
           </div>
           <div className="field mt-4">
