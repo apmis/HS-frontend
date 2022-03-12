86ced3ea6c91db717a4bd8573163704f2060ea43
arabambi <arababs2015@gmail.com>
2022-03-12 10:29:59 +0100

    fluid intake form

--- frontend/src/clientForm/forms/fluidIntake.jsx
+++ frontend/src/clientForm/forms/fluidIntake.jsx
@@ -13,13 +13,13 @@ const FluidIntakeOutput = ({onSubmit}) => {
           <div className="field">
             <label className="label is-small">Patient Name</label>
             <p className="control is-expanded">
-              <input className="input is-small" type="text" />
+              <input ref={register} name="patientName" className="input is-small" type="text" />
             </p>
           </div>
           <div className="field">
             <label className="label is-small">Ward</label>
             <p className="control is-expanded">
-              <input className="input is-small" type="number" />
+              <input ref={register} name="ward" className="input is-small" type="number" />
             </p>
           </div>
           <label className="label is-small">Intake</label>
@@ -28,7 +28,7 @@ const FluidIntakeOutput = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Date & Time</label>
                 <p className="control is-expanded">
-                  <input className="input is-small" type="date" />
+                  <input ref={register} name="dateAndTime" className="input is-small" type="date" />
                 </p>
               </div>
             </div>
@@ -36,7 +36,7 @@ const FluidIntakeOutput = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Oral</label>
                 <p className="control is-expanded">
-                  <input className="input is-small" type="text" />
+                  <input ref={register} name="oral" className="input is-small" type="text" />
                 </p>
               </div>
             </div>
@@ -44,7 +44,7 @@ const FluidIntakeOutput = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Others</label>
                 <p className="control is-expanded">
-                  <input className="input is-small" type="text" />
+                  <input ref={register} name="others1" className="input is-small" type="text" />
                 </p>
               </div>
             </div>
@@ -52,7 +52,7 @@ const FluidIntakeOutput = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Amount</label>
                 <p className="control is-expanded">
-                  <input className="input is-small" type="text" />
+                  <input ref={register} name="amount" className="input is-small" type="text" />
                 </p>
               </div>
             </div>
@@ -63,7 +63,7 @@ const FluidIntakeOutput = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Voided</label>
                 <p className="control is-expanded">
-                  <input className="input is-small" type="text" />
+                  <input ref={register} name="voided" className="input is-small" type="text" />
                 </p>
               </div>
             </div>
@@ -71,7 +71,7 @@ const FluidIntakeOutput = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Catheter</label>
                 <p className="control is-expanded">
-                  <input className="input is-small" type="text" />
+                  <input ref={register} name="catheter" className="input is-small" type="text" />
                 </p>
               </div>
             </div>
@@ -79,7 +79,7 @@ const FluidIntakeOutput = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Stool</label>
                 <p className="control is-expanded">
-                  <input className="input is-small" type="text" />
+                  <input ref={register} name="stool" className="input is-small" type="text" />
                 </p>
               </div>
             </div>
@@ -87,7 +87,7 @@ const FluidIntakeOutput = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Vomitus</label>
                 <p className="control is-expanded">
-                  <input className="input is-small" type="text" />
+                  <input ref={register} name="vomitus" className="input is-small" type="text" />
                 </p>
               </div>
             </div>
@@ -96,7 +96,7 @@ const FluidIntakeOutput = ({onSubmit}) => {
             <div className="field">
               <label className="label is-small">Others</label>
               <div className="control">
-                <textarea className="textarea is-small"></textarea>
+                <textarea ref={register} name="others2" className="textarea is-small"></textarea>
               </div>
             </div>
           </div>
