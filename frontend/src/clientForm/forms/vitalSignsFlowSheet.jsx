423ac796110ad87e810d323aac868995a7a7b4b0
arabambi <arababs2015@gmail.com>
2022-03-18 14:34:49 +0100

    vitalsignsflowsheet

--- frontend/src/clientForm/forms/vitalSignsFlowSheet.jsx
+++ frontend/src/clientForm/forms/vitalSignsFlowSheet.jsx
@@ -13,7 +13,7 @@ const VitalSignsFlowSheet = ({onSubmit}) => {
           <div className="field">
             <label className="label is-small">Patient</label>
             <p className="control is-expanded">
-              <input ref={register} name="" className="input is-small" type="text" />
+              <input ref={register} name="patientName" className="input is-small" type="text" />
             </p>
           </div>
           <div className="columns mt-3">
@@ -21,15 +21,15 @@ const VitalSignsFlowSheet = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">D.O.B</label>
                 <p className="control is-expanded">
-                  <input ref={register} name="" className="input is-small" type="date" />
+                  <input ref={register} name="dateOfBirth" className="input is-small" type="date" />
                 </p>
               </div>
             </div>
             <div className="column">
               <div className="field">
-                <label className="label is-small">M/F</label>
+                <label className="label is-small">Gender</label>
                 <p className="control is-expanded">
-                  <input ref={register} name="" className="input is-small" type="text" />
+                  <input ref={register} name="gender" className="input is-small" type="text" />
                 </p>
               </div>
             </div>
@@ -37,7 +37,7 @@ const VitalSignsFlowSheet = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Physician</label>
                 <p className="control is-expanded">
-                  <input ref={register} name="" className="input is-small" type="text" />
+                  <input ref={register} name="physician" className="input is-small" type="text" />
                 </p>
               </div>
             </div>
@@ -46,7 +46,7 @@ const VitalSignsFlowSheet = ({onSubmit}) => {
             <div className="field">
               <label className="label is-small">Notes</label>
               <div className="control">
-                <textarea ref={register} name="" className="textarea is-small"></textarea>
+                <textarea ref={register} name="notes" className="textarea is-small"></textarea>
               </div>
             </div>
           </div>
@@ -55,7 +55,7 @@ const VitalSignsFlowSheet = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Date</label>
                 <div className="control">
-                  <input ref={register} name="" className="input is-small" type="date" />
+                  <input ref={register} name="date" className="input is-small" type="date" />
                 </div>
               </div>
             </div>
@@ -63,7 +63,7 @@ const VitalSignsFlowSheet = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Weight</label>
                 <div className="control">
-                  <input ref={register} name="" className="input is-small" type="number" />
+                  <input ref={register} name="weight" className="input is-small" type="number" />
                 </div>
               </div>
             </div>
@@ -71,7 +71,7 @@ const VitalSignsFlowSheet = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">BP</label>
                 <div className="control">
-                  <input ref={register} name="" className="input is-small" type="number" />
+                  <input ref={register} name="bloodPressure" className="input is-small" type="number" />
                 </div>
               </div>
             </div>
@@ -79,7 +79,7 @@ const VitalSignsFlowSheet = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Pulse</label>
                 <div className="control">
-                  <input ref={register} name="" className="input is-small" type="text" />
+                  <input ref={register} name="pulse" className="input is-small" type="text" />
                 </div>
               </div>
             </div>
@@ -89,7 +89,7 @@ const VitalSignsFlowSheet = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Respiration</label>
                 <div className="control">
-                  <input ref={register} name="" className="input is-small" type="text" />
+                  <input ref={register} name="respiration" className="input is-small" type="text" />
                 </div>
               </div>
             </div>
@@ -97,7 +97,7 @@ const VitalSignsFlowSheet = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Pain</label>
                 <div className="control">
-                  <input ref={register} name="" className="input is-small" type="text" />
+                  <input ref={register} name="pain" className="input is-small" type="text" />
                 </div>
               </div>
             </div>
@@ -105,7 +105,7 @@ const VitalSignsFlowSheet = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">Initials</label>
                 <div className="control">
-                  <input ref={register} name="" className="input is-small" type="text" />
+                  <input ref={register} name="initials" className="input is-small" type="text" />
                 </div>
               </div>
             </div>
@@ -113,7 +113,7 @@ const VitalSignsFlowSheet = ({onSubmit}) => {
               <div className="field">
                 <label className="label is-small">O2 Sat</label>
                 <div className="control">
-                  <input ref={register} name="" className="input is-small" type="text" />
+                  <input ref={register} name="02sat" className="input is-small" type="text" />
                 </div>
               </div>
             </div>
